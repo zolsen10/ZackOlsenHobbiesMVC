dbe0036aae2e2f8303e77c0aaab7bdb3d2cf9e46
zolsen10 <soccerzack10@yahoo.com>
2024-02-08 11:53:37 -0700

    Add project files.

---
+++ ZackOlsenHobbiesMVC/wwwroot/js/site.js
@@ -0,0 +1,15 @@
+﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+
+const slider = document.querySelector('.slider');
+
+function activate(e) {
+    const items = document.querySelectorAll('.item');
+    e.target.matches('.next') && slider.append(items[0])
+    e.target.matches('.prev') && slider.prepend(items[items.length - 1]);
+}
+
+document.addEventListener('click', activate, false);
+
